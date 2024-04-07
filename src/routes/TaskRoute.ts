6ca48e62d61616457af6c6b0c7584a55323dc332
TanKhang <doank3442@gmail.com>
2024-04-07 22:25:38 +0700

    Squashed commit of the following:
    commit 91d368bdacf2ef6a1353f5140a7aee5d2c8ca8f2
    Author: TanKhang <doank3442@gmail.com>
    Date:   Sun Apr 7 22:22:11 2024 +0700
    
        Fix - Fix Conflict
    
    commit fa45cacc1630d8e687be99abcf89bab2e84107e4
    Author: TanKhang <doank3442@gmail.com>
    Date:   Sun Apr 7 22:20:58 2024 +0700
    
        Squashed commit of the following:
    
        commit a7e09fa96376c97de1a54509d12192f81ae5b6b6
        Author: TanKhang <doank3442@gmail.com>
        Date:   Sun Apr 7 22:10:14 2024 +0700
    
            Fix - Fix Conflict
    
        commit 564d0f6fc45fe0d214be48c65aeecc2dd211042f
        Author: TanKhang <doank3442@gmail.com>
        Date:   Sun Apr 7 22:06:22 2024 +0700
    
            Squashed commit of the following:
    
            commit 3eca6273fb3d59900cb36b249011cbc1a253332b
            Merge: 2d030ca def62c9
            Author: Đoàn Tấn Khang <74121720+tankhang1@users.noreply.github.com>
            Date:   Sun Apr 7 21:59:41 2024 +0700
    
                Merge pull request #3 from tankhang1/getAPI
    
                Get api
    
            commit def62c98030743d8eebb9ded97fbe1469057345c
            Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
            Date:   Sun Apr 7 21:43:54 2024 +0700
    
                add dist when run code
    
            commit b8f13dd4da93bb08cfb622fa90c24aa1d87efdf5
            Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
            Date:   Sat Apr 6 09:34:27 2024 +0700
    
                write api getTasksbyUserId and SearchTaskByText
    
        commit ce021b7a89cc779333063af85f7ad749e2def91c
        Author: thuong22092k3 <everything22092k3@gmail.com>
        Date:   Sun Apr 7 21:03:54 2024 +0700
    
            Delete a task
    
    commit 67c27a88b4fdfe1c445422d472441bbca3e5272f
    Author: TanKhang <doank3442@gmail.com>
    Date:   Sun Apr 7 22:18:40 2024 +0700
    
        Squashed commit of the following:
    
        commit a7e09fa96376c97de1a54509d12192f81ae5b6b6
        Author: TanKhang <doank3442@gmail.com>
        Date:   Sun Apr 7 22:10:14 2024 +0700
    
            Fix - Fix Conflict
    
        commit 564d0f6fc45fe0d214be48c65aeecc2dd211042f
        Author: TanKhang <doank3442@gmail.com>
        Date:   Sun Apr 7 22:06:22 2024 +0700
    
            Squashed commit of the following:
    
            commit 3eca6273fb3d59900cb36b249011cbc1a253332b
            Merge: 2d030ca def62c9
            Author: Đoàn Tấn Khang <74121720+tankhang1@users.noreply.github.com>
            Date:   Sun Apr 7 21:59:41 2024 +0700
    
                Merge pull request #3 from tankhang1/getAPI
    
                Get api
    
            commit def62c98030743d8eebb9ded97fbe1469057345c
            Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
            Date:   Sun Apr 7 21:43:54 2024 +0700
    
                add dist when run code
    
            commit b8f13dd4da93bb08cfb622fa90c24aa1d87efdf5
            Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
            Date:   Sat Apr 6 09:34:27 2024 +0700
    
                write api getTasksbyUserId and SearchTaskByText
    
        commit ce021b7a89cc779333063af85f7ad749e2def91c
        Author: thuong22092k3 <everything22092k3@gmail.com>
        Date:   Sun Apr 7 21:03:54 2024 +0700
    
            Delete a task
    
    commit 3eca6273fb3d59900cb36b249011cbc1a253332b
    Merge: 2d030ca def62c9
    Author: Đoàn Tấn Khang <74121720+tankhang1@users.noreply.github.com>
    Date:   Sun Apr 7 21:59:41 2024 +0700
    
        Merge pull request #3 from tankhang1/getAPI
    
        Get api
    
    commit def62c98030743d8eebb9ded97fbe1469057345c
    Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
    Date:   Sun Apr 7 21:43:54 2024 +0700
    
        add dist when run code
    
    commit b8f13dd4da93bb08cfb622fa90c24aa1d87efdf5
    Author: Mieu4621 <93714175+Mieu4621@users.noreply.github.com>
    Date:   Sat Apr 6 09:34:27 2024 +0700
    
        write api getTasksbyUserId and SearchTaskByText

--- src/routes/TaskRoute.ts
+++ src/routes/TaskRoute.ts
@@ -34,13 +34,3 @@ router.get("/tasks/search", async (req: Request, res: Response) => {
 });
 
 export default router;
-import express, { Express, Request, Response, Router } from "express";
-import { addTask } from "../services/TaskService/PostTask";
-import { updateTask } from "../services/TaskService/UpdateTask";
-
-const router: Router = express.Router();
-
-router.post("/add", addTask);
-router.post("/update", updateTask);
-
-export default router;
